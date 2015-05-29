e0039b4a9e3ea7efeeeadd8da4df7faf2e381a4a
geoffdavis92 <geoffdavis92@gmail.com>
2015-05-28 23:21:27 -0500

    Updated main .js to app.js; update package.json

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req,res) {
+	res.send('!!');
+});
+
+var server = app.listen(8008, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s',host,port);
+})
